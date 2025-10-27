d8e7c79e56bd51ae527ab485e8210a81610e4746
jhj1111 <happyijun@naver.com>
2025-10-28 05:10:43 +0900

    feat(common): create UI Component(CommonArticlePreviewList, CommonSearchBar, CommonSegmentedButton)

---
+++ feature/common/src/main/java/com/sesac/common/component/CommonSearchBar.kt
@@ -0,0 +1,153 @@
+package com.sesac.common.component
+
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.traversalIndex
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import com.sesac.common.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ColumnScope.CommonSearchBar(
+    alignment: Alignment.Horizontal,
+    textFieldState: TextFieldState,
+    onSearch: (String) -> Unit,
+    searchResults: List<String>,
+    searchResultsScrollState: ScrollState = rememberScrollState(),
+    space: Dp = dimensionResource(R.dimen.default_space),
+) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    SearchBar(
+        modifier = Modifier
+            .align(alignment)
+            .fillMaxWidth()
+            .padding(space)
+            .semantics { traversalIndex = -1f },
+        colors = SearchBarDefaults.colors(inputFieldColors = TextFieldDefaults.colors(Color.Black)),
+        inputField = {
+            SearchBarDefaults.InputField(
+                query = textFieldState.text.toString(),
+                onQueryChange = { textFieldState.edit { replace(0, Int.MAX_VALUE, it) } },
+                onSearch = {
+                    onSearch(textFieldState.text.toString())
+                    expanded = false
+                },
+                expanded = expanded,
+                onExpandedChange = { expanded = it },
+                placeholder = { Text("Search") }
+            )
+        },
+        expanded = expanded,
+        onExpandedChange = { expanded = it }
+    ) {
+        // Display search results in a scrollable column
+        Column(Modifier.verticalScroll(searchResultsScrollState)) {
+            searchResults.forEach { result ->
+                ListItem(
+                    headlineContent = { Text(result) },
+                    modifier = Modifier
+                        .clickable {
+                            textFieldState.edit { replace(0, length, result) }
+                            expanded = false
+                        }
+                        .fillMaxWidth()
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BoxScope.CommonSearchBar(
+    alignment: Alignment,
+    textFieldState: TextFieldState,
+    onSearch: (String) -> Unit,
+    searchResults: List<String>,
+    searchResultsScrollState: ScrollState = rememberScrollState(),
+    space: Dp = dimensionResource(R.dimen.default_space),
+) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    SearchBar(
+        modifier = Modifier
+            .align(alignment)
+            .fillMaxWidth()
+            .padding(space)
+            .semantics { traversalIndex = -1f },
+        colors = SearchBarDefaults.colors(inputFieldColors = TextFieldDefaults.colors(Color.Black)),
+        inputField = {
+            SearchBarDefaults.InputField(
+                query = textFieldState.text.toString(),
+                onQueryChange = { textFieldState.edit { replace(0, Int.MAX_VALUE, it) } },
+                onSearch = {
+                    onSearch(textFieldState.text.toString())
+                    expanded = false
+                },
+                expanded = expanded,
+                onExpandedChange = { expanded = it },
+                placeholder = { Text("Search") }
+            )
+        },
+        expanded = expanded,
+        onExpandedChange = { expanded = it }
+    ) {
+        // Display search results in a scrollable column
+        Column(Modifier.verticalScroll(searchResultsScrollState)) {
+            searchResults.forEach { result ->
+                ListItem(
+                    headlineContent = { Text(result) },
+                    modifier = Modifier
+                        .clickable {
+                            textFieldState.edit { replace(0, length, result) }
+                            expanded = false
+                        }
+                        .fillMaxWidth()
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CommonSearchBarPreview(){
+    val onSearch = {s: String -> Unit }
+    MaterialTheme {
+        Column {
+            CommonSearchBar(
+                Alignment.CenterHorizontally,
+                TextFieldState(),
+                onSearch,
+                listOf("a", "b", "c")
+                )
+        }
+    }
+}
---
+++ feature/common/src/main/java/com/sesac/common/component/CommonSegmentedButton.kt
@@ -0,0 +1,89 @@
+package com.sesac.common.component
+
+import android.annotation.SuppressLint
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Preview
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.sesac.common.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CommonSegmentedButton(
+    tabOptions: List<String>,
+    tabSelectedIndex: MutableState<Int>,
+    checkedIcons: List<ImageVector>,
+    unCheckedIcons: List<ImageVector>,
+    textSpace: Dp = dimensionResource(R.dimen.text_top_bottom_space),
+    space: Dp = dimensionResource(R.dimen.default_space),
+) {
+    FlowRow(
+        Modifier
+            .padding(horizontal = space, vertical = textSpace)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Absolute.Center,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        SingleChoiceSegmentedButtonRow {
+            tabOptions.forEachIndexed { index, string ->
+                SegmentedButton(
+                    modifier = Modifier
+                        .wrapContentWidth()
+                        .width(100.dp),
+                    shape = SegmentedButtonDefaults.itemShape(
+                        index = index,
+                        count = tabOptions.size
+                    ),
+                    icon = {
+                        Icon(
+                            imageVector = if (tabSelectedIndex.value == index) checkedIcons[index] else unCheckedIcons[index],
+                            contentDescription = string
+                        )
+                    },
+                    onClick = { tabSelectedIndex.value = index },
+                    selected = index == tabSelectedIndex.value,
+                    label = { Text(string) }
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CommonSegmentedButtonPreview() {
+    MaterialTheme {
+        val tabSelectedIndex = rememberSaveable { mutableStateOf(0) }
+        CommonSegmentedButton(
+            listOf("a", "b", "c"),
+            tabSelectedIndex,
+            listOf(Icons.Default.Preview),
+            listOf(Icons.Default.Star),
+        )
+    }
+}
