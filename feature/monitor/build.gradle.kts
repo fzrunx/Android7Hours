cc168c93ee08e5c571f1c3bc20340efd289e9ba8
fzrunx <fzrunx@gmail.com>
2025-10-27 15:44:25 +0900

    [feat] MonitorScreen UI 화면 추가
    MonitorScreen 부분 UI 추가

--- feature/monitor/build.gradle.kts
+++ feature/monitor/build.gradle.kts
@@ -1,21 +1,20 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
 android {
-    namespace = "com.sesac.android7hours"
-    compileSdk = 36
+    namespace = "com.sesac.monitor"
+    compileSdk {
+        version = release(36)
+    }
 
     defaultConfig {
-        applicationId = "com.sesac.android7hours"
         minSdk = 24
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -44,25 +43,24 @@ android {
 
 dependencies {
 
-    implementation(project(":feature"))
-    implementation(project(":feature:"))
+    implementation(project(":domain"))
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.play.services.maps)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
 
     implementation("androidx.compose.material:material-icons-extended")
-    
 }
---
+++ feature/monitor/src/main/java/com/sesac/monitor/presentation/MonitorCamScreen.kt
@@ -0,0 +1,153 @@
+package com.sesac.monitor.presentation
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.FiberManualRecord
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+
+@Composable
+fun MonitorCamScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.Top
+        ) {
+            CustomButton(
+                buttonLabels = listOf("영상", "GPS")
+            ) { label ->
+                when (label) {
+                    "영상" -> { /* 영상 화면으로 이동 */ }
+                    "GPS" -> { /* GPS 화면으로 이동 */ }
+                }
+            }
+        }
+        Spacer(Modifier.height(24.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1 / 1f)
+                .background(Color.Gray, shape = RoundedCornerShape(8.dp)),
+            contentAlignment = Alignment.Center
+
+        ){
+            Text(
+                text = "영상 영역",
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+
+        }
+        Spacer(Modifier.height(60.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                onClick = { /* TODO: 재생 기능 */ },
+                modifier = Modifier.size(70.dp),
+                colors = ButtonDefaults
+                    .buttonColors(
+                        containerColor = Color(0xFFADFB95),
+                        contentColor = Color.Black
+                    )
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.PlayArrow,
+                    contentDescription = "재생",
+                    modifier = Modifier.size(36.dp)
+                )
+
+            }
+            Button(
+                onClick = { /* TODO: 녹화 기능 */ },
+                modifier = Modifier.size(70.dp),
+                colors = ButtonDefaults
+                    .buttonColors(
+                        containerColor = Color(0xFFFB9898),
+                        contentColor = Color.Black
+                    )
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.FiberManualRecord,
+                    contentDescription = "녹화",
+                    modifier = Modifier.size(36.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomButton(buttonLabels: List<String>, modifier: Modifier = Modifier, onClick: (label: String) -> Unit = {}) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(15.dp) // 버튼 사이 간격
+    ) {
+        buttonLabels.forEach { label ->
+            Button(
+                onClick = { onClick(label) },
+                modifier = modifier
+                    .weight(1f)
+                    .height(60.dp),
+                shape = RoundedCornerShape(16.dp),
+                colors = ButtonDefaults
+                    .buttonColors(
+                        containerColor = Color.Gray,
+                        contentColor = Color.White
+                    ),
+                elevation = ButtonDefaults.buttonElevation(defaultElevation = 8.dp)
+            ) {
+                Text(
+                    text = label,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MonitorCamScreenPreview() {
+        MonitorCamScreen()
+}
+
