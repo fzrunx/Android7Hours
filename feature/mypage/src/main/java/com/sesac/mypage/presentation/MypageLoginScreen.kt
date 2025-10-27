11b88bb19ed9727206295c6f3e2ca3a0a7fd17a8
fzrunx <fzrunx@gmail.com>
2025-10-27 19:49:01 +0900

    feat (Mypage): create MypageJoinScreen.kt, MypageLoginScreen.kt, MypageManageScreen.kt UI

--- feature/mypage/src/main/java/com/sesac/mypage/presentation/MypageLoginScreen.kt
+++ feature/mypage/src/main/java/com/sesac/mypage/presentation/MypageLoginScreen.kt
@@ -1,25 +1,15 @@
-package com.example.mypage
+package com.sesac.mypage.presentation
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Comment
-import androidx.compose.material.icons.filled.Hiking
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Monitor
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
