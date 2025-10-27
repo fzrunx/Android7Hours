a6d6b0ff1b2a421007a78e725be781995c0cfb09
jhj1111 <happyijun@naver.com>
2025-10-27 11:40:17 +0900

    feat(home): add build.gradle compose dependencies

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -41,12 +41,30 @@ android {
 
 dependencies {
 
-    implementation(project(":domain"))
+//    implementation(project(":domain"))
+    implementation(project(":feature:common"))
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    
+    // compose
     implementation(libs.material)
+    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
