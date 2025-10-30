c6afa0586b1d3539efcfd782c7f4305632140a96
jhj1111 <happyijun@naver.com>
2025-10-30 10:07:25 +0900

    feat(nav): add 각 기능 별 main 화면 navigation

--- app/src/main/java/com/sesac/android7hours/MainActivity.kt
+++ app/src/main/java/com/sesac/android7hours/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.sesac.common.ui.theme.Android7HoursTheme
-import com.sesac.android7hours.common.EntryPointScreen
+import com.sesac.android7hours.nav_graph.EntryPointScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
