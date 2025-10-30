119e72c5a28427548bb99c245c8d1ed812801be7
jhj1111 <happyijun@naver.com>
2025-10-30 17:43:52 +0900

    feat(setting): add Application 세팅(coil)

--- app/src/main/java/com/sesac/android7hours/common/A7HApplication.kt
+++ app/src/main/java/com/sesac/android7hours/common/A7HApplication.kt
@@ -1,6 +1,12 @@
 package com.sesac.android7hours.common
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.disk.directory
+import coil3.memory.MemoryCache
 
 class A7HApplication(): Application() {
     override fun onCreate() {
@@ -11,4 +17,35 @@ class A7HApplication(): Application() {
         private lateinit var a7HApp: A7HApplication
         fun getA7HApplication() = a7HApp
     }
+}
+
+//Default Memory Size = 0.15 ~ 0.2
+const val COIL_MEMORY_CACHE_SIZE_PERCENT = 0.3
+
+//Coil Disk Cache Size Setting
+//30~100
+const val COIL_DISK_CACHE_DIR_NAME = "coil_file_cache"
+const val COIL_DISK_CACHE_MAX_SIZE = 1024 * 1024 * 100
+
+class CoilUsingApplication : Application(), SingletonImageLoader.Factory {
+    override fun onCreate() {
+        super.onCreate()
+        myApplication = this
+    }
+    companion object{
+        private lateinit var myApplication: CoilUsingApplication
+        fun getCoilUsingApplication() = myApplication
+    }
+    override fun newImageLoader(context: PlatformContext): ImageLoader = ImageLoader.Builder(context)
+        .memoryCache {
+            MemoryCache.Builder()
+                .maxSizePercent(context, COIL_MEMORY_CACHE_SIZE_PERCENT)
+                .build()
+        }
+        .diskCache {
+            DiskCache.Builder()
+                .directory(filesDir.resolve(COIL_DISK_CACHE_DIR_NAME))
+                .maximumMaxSizeBytes(COIL_DISK_CACHE_MAX_SIZE.toLong())
+                .build()
+        }.build()
 }
